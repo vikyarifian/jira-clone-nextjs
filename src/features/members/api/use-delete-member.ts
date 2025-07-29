974c877f0172f81bc305bbeeb9f68d5833ade53a
Viky Arifiansyah <vikyarifiansyah@gmail.com>
2025-07-29 18:08:44 +0700

    delete-&-update-member

---
+++ src/features/members/api/use-delete-member.ts
@@ -0,0 +1,32 @@
+import { toast } from "sonner";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { client } from "@/lib/rpc";
+
+type ResponseType = InferResponseType<typeof client.api.members[":memberId"]["$delete"], 200>;
+type RequestType = InferRequestType<typeof client.api.members[":memberId"]["$delete"]>;
+
+export const useDeleteMember = () => {
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation<ResponseType, Error, RequestType>({
+        mutationFn: async ({ param }) => {
+            const response = await client.api.members[":memberId"]["$delete"]({ param });
+
+            if (!response.ok) {
+                throw new Error("Member delete failed");
+            }   
+
+            return await response.json();
+        },
+        onSuccess: () => {
+            toast.success("Member delete successfully", { style: { color: "green" } });
+            queryClient.invalidateQueries({ queryKey: ["members"] });
+        },
+        onError: (error) => {
+            toast.error(`Failed to delete member`, { style: { color: "red" } });
+        },
+    });
+
+    return mutation;
+};
--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -44,6 +44,9 @@ export async function createAdminClient() {
     return {
         get account() { 
             return new Account(client);
+        },
+        get users() {
+            return new Users(client);
         }
     };
 };
