33279b5befbde00e7dd8da313eda9a232e5f7de3
Viky Arifiansyah <vikyarifiansyah@gmail.com>
2025-07-30 15:31:55 +0700

    add-workspace-project

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,8 @@
+import { CreateProjectModal } from "@/features/projects/components/create-project-modal";
+import { CreateWorkspaceModal } from "@/features/workspaces/components/create-workspace-modal";
+
 import { Navbar } from "@/components/navbar";
 import { Sidebar } from "@/components/sidebar";
-import { CreateWorkspaceModal } from "@/features/workspaces/components/create-workspace-modal";
 
 interface DashboardLayoutProps {
     children: React.ReactNode;
@@ -10,6 +12,7 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <div className="min-h-screen">
         <CreateWorkspaceModal />
+        <CreateProjectModal />
         <div className="flex w-full h-full">
             <div className="fixed left-0 top-0 hidden lg:block lg:w-[264px] h-full overflow-y-auto">
                 <Sidebar />
---
+++ src/features/projects/components/create-project-form.tsx
@@ -0,0 +1,192 @@
+"use client";
+
+import z from "zod";
+import { useRef } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useForm } from "react-hook-form";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { createProjectSchema } from "../schemas";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { DottedSeparator } from "@/components/dotted-separator";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useCreateProject } from "../api/use-create-project";
+import Image from "next/image";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { ImageIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+interface CreateProjectFormProps {
+    onCancel?: () => void;
+};
+
+export const CreateProjectForm = ({ onCancel }: CreateProjectFormProps) => {
+    const workspaceId = useWorkspaceId();
+    const router = useRouter();
+    const { mutate, isPending } = useCreateProject();
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const form = useForm<z.infer<typeof createProjectSchema>>({
+        resolver: zodResolver(createProjectSchema.omit({ workspaceId: true })),
+        defaultValues: {
+            name: "",
+        },
+    });
+
+    const onSubmit = (values: z.infer<typeof createProjectSchema>) => {
+        const finalValues = {
+            ...values,
+            workspaceId,
+            image: values.image instanceof File ? values.image : "",
+        };  
+
+        mutate({ form: finalValues }, {
+            onSuccess: ({ data }) => {
+                form.reset();
+                // onCancel?.();
+                // router.push(`/projects/${data.$id}`);
+            },
+        });
+    };
+
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (file) {
+            form.setValue("image", file);
+        }
+    };
+
+    return (
+        <Card className="w-full h-full border-none shadow-none">
+            <CardHeader className="flex p-7">
+                <CardTitle className="text-2xl font-bold">
+                    Create a new Project
+                </CardTitle>
+            </CardHeader>
+            <div className="p-7">
+                <DottedSeparator />
+            </div>
+            <CardContent className="p-7">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)}>
+                        <div className="flex flex-col gap-y-4">
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>
+                                            Project Name
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                {...field}
+                                                type="text"
+                                                placeholder="Enter Project Name"
+                                                />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="image"
+                                render={({ field }) => (
+                                    <div className="flex flex-col gap-y-2">
+                                        <div className="flex items-center gap-x-5">
+                                            {field.value ? (
+                                                <div className="size-[72px] relative rounded-md overflow-hidden">
+                                                    <Image 
+                                                        alt="Project Image"
+                                                        fill
+                                                        className="object-cover"
+                                                        src={
+                                                        field.value instanceof File
+                                                        ? URL.createObjectURL(field.value)
+                                                        : field.value
+                                                    } />
+                                                </div>
+                                            )
+                                            :
+                                            (
+                                                <Avatar className="size-[72px]">
+                                                    <AvatarFallback>
+                                                        <ImageIcon className="size-[36px] text-neutral-400" />
+                                                    </AvatarFallback>
+                                                </Avatar>
+                                            )}
+                                            <div className="flex flex-col">
+                                                <p className="text-sm">Project Icon</p>
+                                                <p className="text-sm text-muted-foreground">
+                                                    JPG, PNG or JPEG (max 1MB)
+                                                </p>
+                                                <input
+                                                    className="hidden"
+                                                    accept=".jpg, .png, .jpeg"
+                                                    type="file"
+                                                    ref={inputRef}
+                                                    disabled={isPending}
+                                                    onChange={handleImageChange}
+                                                 />
+                                                 {field.value ? (
+                                                    <Button
+                                                        type="button"   
+                                                        size="xs"
+                                                        variant="destructive"
+                                                        onClick={() => {
+                                                            field.onChange(null);
+                                                            if (inputRef.current) {
+                                                                inputRef.current.value = "";
+                                                            }
+                                                        }}
+                                                        disabled={isPending}
+                                                    >Remove Image</Button>
+                                                 ) : (
+                                                    <Button
+                                                        type="button"   
+                                                        size="xs"
+                                                        variant="teritary"
+                                                        onClick={() => inputRef.current?.click()}
+                                                        disabled={isPending}
+                                                    >Upload Image</Button>
+                                                 )}
+                                            </div>
+                                        </div>
+                                    </div>
+                                )}
+                            />
+                        </div>
+                        <DottedSeparator className="py-7" />
+                        <div className="flex items-center justify-between">
+                            <Button
+                                type="button"
+                                size={"lg"}
+                                variant={"secondary"}
+                                onClick={onCancel}
+                                disabled={isPending}
+                                className={cn(
+                                    !onCancel && "invisible"
+                                )}
+                            >
+                                Cancel
+                            </Button>
+                            <Button
+                                type="submit"
+                                size={"lg"}
+                                disabled={isPending}
+                            >
+                                Create Project
+                            </Button>
+                        </div>
+                    </form>
+                </Form>
+            </CardContent>
+        </Card>
+    )
+}
