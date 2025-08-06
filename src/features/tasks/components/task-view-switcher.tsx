bf3660db1c01e3ae2712d6872846b6ed332af0f4
Viky Arifiansyah <vikyarifiansyah@gmail.com>
2025-08-06 19:57:27 +0700

    create-task-form

---
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { PlusIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { 
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger
+} from "@/components/ui/tabs";
+import { DottedSeparator } from "@/components/dotted-separator";
+
+import { useCreateTaskModal } from "../hooks/use-create-task-modal";
+
+export const TaskViewSwitcher = () => {
+    const { isOpen, open } = useCreateTaskModal();
+    
+    return (
+        <Tabs className="flex-1 w-full border rounded-lg">
+            <div className="h-full flex flex-col overflow-auto p-4">                
+                <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
+                    <TabsList className="w-full lg:w-auto">
+                        <TabsTrigger 
+                            className="h-8 w-full lg:w-auto"
+                            value="table"
+                        >
+                            Table
+                        </TabsTrigger>
+                        <TabsTrigger 
+                            className="h-8 w-full lg:w-auto"
+                            value="kanban"
+                        >
+                            Kanban
+                        </TabsTrigger>
+                        <TabsTrigger 
+                            className="h-8 w-full lg:w-auto"
+                            value="calendar"
+                        >
+                            Calendar
+                        </TabsTrigger>
+                    </TabsList>
+                    <Button
+                        onClick={open}
+                        size="sm"
+                        className="w-full lg:w-auto"
+                    >
+                        <PlusIcon className="size-4 mr-2" />
+                        New Task
+                    </Button>
+                </div>
+                <DottedSeparator className="my-4" />
+                Data Filters
+                <DottedSeparator className="my-4" />
+                <>
+                    <TabsContent value="table" className="mt-0">
+                        Data table
+                    </TabsContent>
+                    <TabsContent value="kanban" className="mt-0">
+                        Data kanban
+                    </TabsContent>
+                    <TabsContent value="calendar" className="mt-0">
+                        Data calendar
+                    </TabsContent>
+                </>
+            </div>
+        </Tabs>
+    );
+};
