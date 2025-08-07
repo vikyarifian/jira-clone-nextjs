aaa80b36c8327e7cf39269e02bc494f725b6f450
Viky Arifiansyah <vikyarifiansyah@gmail.com>
2025-08-07 18:44:18 +0700

    add-data-table

--- src/features/tasks/api/use-get-tasks.ts
+++ src/features/tasks/api/use-get-tasks.ts
@@ -2,18 +2,45 @@ import { useQuery } from "@tanstack/react-query";
 
 import { client } from "@/lib/rpc";
 
+import { TaskStatus } from "../types";
+
 interface UseGetTasksProps {
     workspaceId: string,
+    projectId?: string | null,
+    status?: TaskStatus | null,
+    assigneeId?: string | null,
+    dueDate?: string | null,
+    search?: string | null,
 }
 
 export const useGetTasks = ({
-    workspaceId
+    workspaceId,
+    projectId,
+    status,
+    assigneeId,
+    dueDate,
+    search,
 }: UseGetTasksProps) => {
     const query = useQuery({
-        queryKey: ["tasks", workspaceId],
+        queryKey: [
+            "tasks", 
+            workspaceId,
+            projectId,
+            status,
+            assigneeId,
+            dueDate,
+            search,
+        ],
         queryFn: async () => {
-            const response = await client.api.projects.$get({
-                query: { workspaceId }
+            const response = await client.api.tasks.$get({
+                query: { 
+                    workspaceId,
+                    projectId: projectId ?? undefined,
+                    status: status ?? undefined,
+                    assigneeId: assigneeId ?? undefined,
+                    dueDate: dueDate ?? undefined,
+                    search: search ?? undefined,
+                }
             });    
 
             if (!response.ok) {
--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { PlusIcon } from "lucide-react";
+import { Loader, PlusIcon } from "lucide-react";
+import { useQueryState } from "nuqs";
 
 import { Button } from "@/components/ui/button";
 import { 
@@ -11,13 +12,46 @@ import {
 } from "@/components/ui/tabs";
 import { DottedSeparator } from "@/components/dotted-separator";
 
+import { columns } from "./columns";
+import { DataTable } from "./data-table";
+import { DataFilters } from "./data-filters";
+
+import { useGetTasks } from "../api/use-get-tasks";
+import { useTaskFilters } from "../hooks/use-task-filters";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { useCreateTaskModal } from "../hooks/use-create-task-modal";
 
 export const TaskViewSwitcher = () => {
-    const { isOpen, open } = useCreateTaskModal();
+    const [{
+        status,
+        assigneeId,
+        projectId,
+        dueDate
+    }] = useTaskFilters();
+    
+    const [ view, setView ] = useQueryState("task-view", {
+        defaultValue: "table",        
+    })
+
+    const workspaceId = useWorkspaceId();
+    const { open } = useCreateTaskModal();
+
+    const { 
+        data: tasks, 
+        isLoading: isLoadingTasks 
+    } = useGetTasks({ 
+        workspaceId,
+        projectId,
+        assigneeId,
+        status,
+        dueDate,
+    });
     
     return (
-        <Tabs className="flex-1 w-full border rounded-lg">
+        <Tabs 
+            defaultValue={view}
+            onValueChange={setView}
+            className="flex-1 w-full border rounded-lg">
             <div className="h-full flex flex-col overflow-auto p-4">                
                 <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
                     <TabsList className="w-full lg:w-auto">
@@ -50,19 +84,26 @@ export const TaskViewSwitcher = () => {
                     </Button>
                 </div>
                 <DottedSeparator className="my-4" />
-                Data Filters
+                <DataFilters />
                 <DottedSeparator className="my-4" />
-                <>
-                    <TabsContent value="table" className="mt-0">
-                        Data table
-                    </TabsContent>
-                    <TabsContent value="kanban" className="mt-0">
-                        Data kanban
-                    </TabsContent>
-                    <TabsContent value="calendar" className="mt-0">
-                        Data calendar
-                    </TabsContent>
-                </>
+                {isLoadingTasks ? (
+                    <div className="w-full border rounded-lg h-[200px] flex flex-col items-center justify-center">
+                        <Loader className="size-5 animate-spin text-muted-foreground" />
+                    </div>
+                ) :
+                (
+                    <>
+                        <TabsContent value="table" className="mt-0">
+                            <DataTable columns={columns} data={tasks?.documents ?? []} />
+                        </TabsContent>
+                        <TabsContent value="kanban" className="mt-0">
+                            {JSON.stringify(tasks)}
+                        </TabsContent>
+                        <TabsContent value="calendar" className="mt-0">
+                            {JSON.stringify(tasks)}
+                        </TabsContent>
+                    </>
+                )}
             </div>
         </Tabs>
     );
